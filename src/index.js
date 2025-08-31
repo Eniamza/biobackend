9dd78e26d55b1785eecc10b447766f6913258870
Eniamza <eniamza0@gmail.com>
2025-08-31 12:09:10 +0600

    feat: add graceeful shutdown

--- src/index.js
+++ src/index.js
@@ -7,9 +7,23 @@ app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port: 3000
 }, (info) => {
   console.log(`Server is running on http://localhost:${info.port}`)
+})
+
+process.on('SIGINT', () => {
+  server.close()
+  process.exit(0)
+})
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
 })
